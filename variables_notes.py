4f345c955a2f033519ee72209bcdf541bd7ce891
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-08-19 09:52:55 -0600

    Variable notes (examples) done, and the character Intro file made

---
+++ variables_notes.py
@@ -0,0 +1,20 @@
+# IB - Variables Notes
+
+# Create a variable
+name = "Katie"
+
+print("Hello " + name)
+
+# reset variable
+name = "Alex"
+
+print("Sorry, I meant " + name)
+
+
+# another variable example
+age = 15
+
+print("Wow, you are", age)
+print("Oh wait, your birthday was yesterday?")
+age += 1
+print("So you are", age)
