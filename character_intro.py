ec8232df42392ed654baf5b3974d8a58ecdb7380
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-08-20 12:46:30 +0000

    Character Intro Nearly completed

--- character_intro.py
+++ character_intro.py
@@ -1,2 +1,8 @@
 # IB - Character Introduction
 
+name = input("What is your character's name? ").strip().title()
+age = input("How old is your character? ").strip()
+job = input("What is your character's job? ").strip().lower()
+hometown = input("Where is your character from? ").strip().title()
+
+print(f"Hi, my name is {name}. I am {age} years old, I work as a {job}, and I'm from {hometown}.")
