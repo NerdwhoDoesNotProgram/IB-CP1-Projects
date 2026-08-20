17f59ed17f38e2a57c956c80fcabbbe670c8437d
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-08-20 09:06:04 -0600

    variable names changed

--- character_intro.py
+++ character_intro.py
@@ -1,8 +1,8 @@
 # IB - Character Introduction
 
-name = input("What is your character's name? ").strip().title()
-age = input("How old is your character? ").strip()
-job = input("What is your character's job? ").strip().lower()
-hometown = input("Where is your character from? ").strip().title()
+char_name = input("What is your character's name? ").strip().title()
+char_age = input("How old is your character? ").strip()
+char_job = input("What is your character's job? ").strip().lower()
+char_hometown = input("Where is your character from? ").strip().title()
 
-print(f"Hi, my name is {name}. I am {age} years old, I work as a {job}, and I'm from {hometown}.")
+print(f"Hi, my name is {char_name}. I am {char_age} years old, I work as a {char_job}, and I'm from {char_hometown}.")
