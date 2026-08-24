e8346ef4edd2961938793f5d9a34c2186e5df337
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-08-24 09:17:56 -0600

    Debugging assigment done

---
+++ debugging.py
@@ -0,0 +1,10 @@
+# IB - Fix The Code
+
+name = "Alex"
+age = 16
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+#                        ^                                          ^
+# Fixed: Incorrect sppelling of a variable,                         |
+# added an 'e' to make the Variable name: name                      |
+#                                               Fixed: Was missing a closing Parenthesis, and I added one.
