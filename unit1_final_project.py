a059f673aeffc4188fa44be54a72148ac329eac0
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-08-26 09:22:00 -0600

    Formatted print statement, added comments

--- unit1_final_project.py
+++ unit1_final_project.py
@@ -2,9 +2,9 @@
 
 name = input("What is your name? ").strip().title()
 
-school = input("What school do you attend? ").strip()
+school = input("What school do you attend? ").strip() #title does not work for acronyms such as UCAS
 
-hobby = input("What is your favorite hobby or activity? ").strip()
+hobby = input("What is your favorite hobby or activity? ").strip() # lowercase?
 
 fav_genre = input("What is your favorite type of book, movie, or game? ").strip().capitalize()
 
@@ -14,6 +14,9 @@ future_goal = input("What is something you would like to accomplish in the futur
 
 personality = input("Describe yourself using one word: ").strip().lower()
 
+"""print(f"\nHi! My name is {name}, and I attend {school}. I would describe myself as {personality}. In my free time, I enjoy {hobby}, and I especially like {fav_genre}. One place I would love to visit is {dream_destination}, and one of my goals for the future is to {future_goal}. Thanks for getting to know me!")"""
+
+# Having separate f strings make writing, editing, and structuring of sentences much easier.
 print(f"\nHi! My name is {name}, and I attend {school}. "
 f"I would describe myself as {personality}. "
 f"In my free time, I enjoy {hobby}, and I especially like {fav_genre}. "
