86ffcacaf164577286c57906ae9b81d061fcba30
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-09-01 09:00:22 -0600

    Notes

---
+++ Notes/integers_floats.py
@@ -0,0 +1,21 @@
+# IB Integers and Floats Notes
+
+# Integer - A whole number
+num = 75 # just write the number, no extra syntax required
+
+#float - A number with a decimal point
+pi = 3.1415 # Just write the number, no extra syntax required
+
+# Arithmetic operators (+, -, *, /, **, //, %)
+print(5/2)# Integer division (Only gives the integer, drops the decimal)
+print(5//2) # Alwats gives you a decimal, even if it is a Zero
+
+# Modulo/mod (modulus)
+print(5%2) # Gives the remainder of a division problem
+print(10%4)
+print(15%5)
+
+print((2-1)*3+4%3)
+# Order of operations (PEMMDAS) Extra M for modulo
+
+# assignment oporator =
