c3c1a34d76c55a7820ed1b80f433af43107cf8b9
NerdwhoDoesNotProgram <isaac.billings@ucas-edu.net>
2026-09-25 17:48:57 +0000

    Still does not work again, but it was. Maybe moving the files messed it up.

--- Usersignin/testing.py
+++ Usersignin/testing.py
@@ -1,30 +1,11 @@
 # IB, 2nd perod - User Sign In
 
-# Aspire?
-# Want a sign up
-#
-#
-#
-#
-#
-#
-#
-"""from plyer import notification
 
-notification.notify(
-    title="Python Alert",
-    message="This is a native computer notification!",
-    app_name="My Python Script",
-    timeout=10 # Duration in seconds the notification stays on screen
-)"""
-
-#
 import random, time, sys
 
-import tkinter as tk
-from tkinter import messagebox
+from plyer import notification
 
-from Projects.user_signin_project.storage import USERS
+from Usersignin.storage import USERS
 
 
 def get_username():
@@ -47,17 +28,13 @@ def generate_code():
 
 def show_code(code):
     try:
-        root = tk.Tk()
-        root.withdraw()
-
-        messagebox.showinfo(
-            "Aspire Verification",
-            f"Your verification code is: {code}"
+        notification.notify(
+            title = "Aspire Verification",
+            message = f"Your verification code is: {code}",
+            app_name = "Aspire",
         )
 
-        #root.destroy()
-
-    except tk.TclError:
+    except (NotImplementedError, Exception) as e:
         print(f"\nYour verification code is: {code}")
 
 def verify_code(code):
